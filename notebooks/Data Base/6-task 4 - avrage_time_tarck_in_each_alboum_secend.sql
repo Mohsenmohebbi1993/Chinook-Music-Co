0be68ab8ade3aa62bb01f538df522bb5b0ec65f3
SQL Mohsen <Mohsen_mohebbi@ymail.com>
2024-07-29 13:26:29 +0330

    edidet

--- notebooks/Data Base/6-task 4 - avrage_time_tarck_in_each_alboum_secend.sql
+++ notebooks/Data Base/6-task 4 - avrage_time_tarck_in_each_alboum_secend.sql
@@ -1,6 +1,6 @@
 select distinct tr.AlbumId,
 	al.Title,
-	avg(tr.Milliseconds/1000) over (partition by tr.AlbumId) as avrage_time_tarck_in_each_alboum_secend
+	avg(tr.Milliseconds/(1000*60)) over (partition by tr.AlbumId) as avrage_time_tarck_in_each_alboum_secend
 	from track tr
 join album al on tr.AlbumId = al.AlbumId
 order by avrage_time_tarck_in_each_alboum_secend desc
